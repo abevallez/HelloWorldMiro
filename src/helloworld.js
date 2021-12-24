9a2a80cc2103df903234250637a38807e38a6c5e
Abraham Vallez <abevallez@gmail.com>
2021-12-24 15:04:30 +0100

    HelloWorldMiro first commit

---
+++ src/helloworld.js
@@ -0,0 +1,26 @@
+const waitOneSec = () => new Promise(r => setTimeout(r, 2000))
+
+function run() {
+    miro.initialize({
+        extensionPoints: {
+          bottomBar: {
+            title: 'Hello World',
+            svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+            onClick: () => {
+              helloWorld()
+            }
+          }
+        }
+      })
+}
+
+function helloWorld() {
+    let sticker = (await miro.board.widgets.create({type:'sticker', text: 'Hello'}))[0]
+    await miro.board.viewport.zoomToObject(sticker)
+    await waitOneSec() // timeout only for demo purpose
+    await miro.board.widgets.update({id: sticker.id, text: 'world!', style:{stickerBackgroundColor:'#7ac673'}}) // update sticker
+    await waitOneSec()
+    await miro.board.widgets.deleteById(sticker.id) // delete sticker
+}
+
+miro.onReady(run)
