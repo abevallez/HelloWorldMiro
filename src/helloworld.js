2d5a74c7c1d2ac1046f3e40f5fb81ab4bf29b6de
Abraham Vallez <abevallez@gmail.com>
2021-12-25 20:27:22 +0100

    fix async function

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,4 +1,5 @@
-const waitOneSec = () => new Promise(r => setTimeout(r, 2000))
+/* eslint-disable require-jsdoc */
+const waitOneSec = () => new Promise((r) => setTimeout(r, 2000));
 
 function run() {
   miro.initialize({
@@ -7,20 +8,20 @@ function run() {
         title: 'Amazon AWS',
         svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         onClick: () => {
-          alert('Hi!')
-        }
-      }
-    }
-  })
+          alert('Hi!');
+        },
+      },
+    },
+  });
 }
 
-function helloWorld() {
-    let sticker = (await miro.board.widgets.create({type:'sticker', text: 'Hello'}))[0]
-    await miro.board.viewport.zoomToObject(sticker)
-    await waitOneSec() // timeout only for demo purpose
-    await miro.board.widgets.update({id: sticker.id, text: 'world!', style:{stickerBackgroundColor:'#7ac673'}}) // update sticker
-    await waitOneSec()
-    await miro.board.widgets.deleteById(sticker.id) // delete sticker
+async function helloWorld() {
+  const sticker = (await miro.board.widgets.create({type: 'sticker', text: 'Hello'}))[0];
+  await miro.board.viewport.zoomToObject(sticker);
+  await waitOneSec(); // timeout only for demo purpose
+  await miro.board.widgets.update({id: sticker.id, text: 'world!', style: {stickerBackgroundColor: '#7ac673'}}); // update sticker
+  await waitOneSec();
+  await miro.board.widgets.deleteById(sticker.id); // delete sticker
 }
 
-miro.onReady(run)
+miro.onReady(run);
